205bb6bca0752e6684bd4f9748051c0b3d41d914
Uzumaki-tushar <tusharsourav01@gmail.com>
2026-08-30 19:08:02 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-jcxge@8cwms-4-$&qia^6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'django-crm-oui6.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
